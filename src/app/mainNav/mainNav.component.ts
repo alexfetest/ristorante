dc98df644028cc9e6f97c0216848225d8ebd87b0
Alex <alex.djokic@gmail.com>
2018-01-08 14:41:02 -0500

    some style refinement

--- src/app/mainNav/mainNav.component.ts
+++ src/app/mainNav/mainNav.component.ts
@@ -3,22 +3,33 @@ import { Component } from '@angular/core';
 const menuItems: {
   icon?: string;
   name: string;
+  link: string;
   active?: boolean;
+  dropdown?: { name: string, link: string }[]
   }[] = [
   {
     name: 'Home',
     icon: 'home',
+    link: '#',
     active: true
   },
   {
     name: 'About',
-    icon: 'info-sign'
+    icon: 'info-sign',
+    link: '#',
  },
  {
-   name: 'Menu'
+   name: 'Menu',
+   link: '#',
+   dropdown: [
+     { name: 'Item 1', link: '#' },
+     { name: 'Item 2', link: '#' },
+     { name: 'Item 3', link: '#' }
+   ]
  },
  {
-   name: 'Contact'
+   name: 'Contact',
+   link: '#',
  }
 ];
 
